d2081c9605c5e92fc0ee35a88ddcdeaed3fe72a6
HLGHT <2457405665@qq.com>
2025-04-30 23:56:17 +0800

    Add files via upload
    init

---
+++ deepledger-boot/src/test/java/com/example/deepledgerboot/DeepledgerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepledgerboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepledgerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
