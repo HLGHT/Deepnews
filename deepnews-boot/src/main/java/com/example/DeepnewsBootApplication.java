c169440d0ec1fcd51eeac1545d7c78133fcdb655
HLGHT <2457405665@qq.com>
2025-05-01 14:05:33 +0800

    init

--- deepnews-boot/src/main/java/com/example/DeepnewsBootApplication.java
+++ deepnews-boot/src/main/java/com/example/DeepnewsBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeepledgerBootApplication {
+public class DeepnewsBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeepledgerBootApplication.class, args);
+        SpringApplication.run(DeepnewsBootApplication.class, args);
     }
 
 }
